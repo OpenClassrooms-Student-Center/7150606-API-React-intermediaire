300dd61397d0470804eb31bed506fa4b319700bc
Ansaroll <andrianiainarolland123@gmail.com>
2023-03-13 22:40:08 +0300

    third comments

--- controllers/freelance.js
+++ controllers/freelance.js
@@ -4,6 +4,7 @@ function getFreelance(id) {
 	return freelancesData.find((freelancer => freelancer.id === id))
 	// Comments
 	// Comments
+	// Comments 3
 	// 1. The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
 }
 
