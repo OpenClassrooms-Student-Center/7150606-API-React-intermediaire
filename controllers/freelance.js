0d00acb9bc738a383799d4d70b3adcc803e333aa
Zonirinaa <zonirinacr7@gmail.com>
2023-03-13 22:39:22 +0300

    Coms

--- controllers/freelance.js
+++ controllers/freelance.js
@@ -3,6 +3,7 @@ const freelancesData = require('../models/freelances')
 function getFreelance(id) {
 	return freelancesData.find((freelancer => freelancer.id === id))
 	// Comments
+	// Comments
 	// 1. The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
 }
 
